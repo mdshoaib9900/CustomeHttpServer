fd7649bee1b71607ac0c2500016ca0a883aba751
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-05-23 16:14:30 +0530

    new commit

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HttpServer {
+    public HttpServer serverEstablishment() throws IOException {
+        int port=8090;
+        ServerSocket serverSocket=new ServerSocket(port);
+        System.out.println("Server started on port " + port);
+
+        while(true){
+            Socket clientSocket=serverSocket.accept();
+            System.out.println("client connected "+clientSocket.getInetAddress());
+
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+            BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            String line;
+            while(!(line=bufferedReader.readLine()).isEmpty()){
+                System.out.println(line);
+            }
+            String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from Shoaib's server!";
+            bufferedWriter.write(response);
+            bufferedWriter.flush();
+            clientSocket.close();
+
+        }
+
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = new HttpServer();
+        server.serverEstablishment();
+
     }
+
 }
