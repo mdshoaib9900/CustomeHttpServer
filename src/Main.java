cb81a0c071dd04a48b49b90532cd86be8b53ae46
mohammedShoaib <130300001+mdshoaib9900@users.noreply.github.com>
2025-05-28 19:01:38 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer server = new HttpServer();
-        server.serverEstablishment();
-
+        try {
+            HttpServer server = new HttpServer(8080);
+            server.serverEstablishment();
+        }catch (IOException e){
+            System.out.println("server error "+e.getMessage());
+        }
     }
 
 }
